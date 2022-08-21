f284cfe1c4cffa7bc6f3953d6ff7747f44b8d1a6
tuba379 <tuba15-13887@diu.edu.bd>
2022-08-22 00:23:47 +0600

    first commit

---
+++ js/deposit.js
@@ -0,0 +1,18 @@
+document.getElementById('deposit-field').addEventListener('keyup', function(event) {
+    document.getElementById('diposit-display').innerText = event.target.value;
+})
+
+document.getElementById('btn-deposit').addEventListener('click', function() {
+    const newDepositAmount = getInputValueById('deposit-field');
+    if (isNaN(newDepositAmount) || newDepositAmount < 0) {
+        alert('ENTER VALID NUMBERS !');
+        return;
+    }
+    const previousDepositTotal = getElementValueById('deposit-amount');
+    const currentDepositAmount = newDepositAmount + previousDepositTotal;
+    setElementValue('deposit-amount', currentDepositAmount);
+    const previousTotalBalance = getElementValueById('total-balance');
+    const currentTotalBalance = newDepositAmount + previousTotalBalance;
+    document.getElementById('diposit-display').innerText = '00';
+    setElementValue('total-balance', currentTotalBalance);
+});
